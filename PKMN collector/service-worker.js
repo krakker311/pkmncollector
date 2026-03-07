6acf5c04f63f4ae3592f452343a840538aff6872
Garry <49022332+krakker311@users.noreply.github.com>
2026-03-07 23:49:37 +0700

    Add caching for offline support in service worker
    Implement caching strategy for service worker

--- PKMN collector/service-worker.js
+++ PKMN collector/service-worker.js
@@ -1,5 +1,27 @@
-self.addEventListener("install", e => {
-console.log("Service Worker installed")
-})
+const CACHE_NAME = "pokemon-cache-v1";
 
-self.addEventListener("fetch", e => {})
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).then(fetchRes => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, fetchRes.clone());
+          return fetchRes;
+        });
+      });
+    })
+  );
+});
